bd9dd582176d043012bfd55f35760f1447e969ed
yehuda24 <158982057+yehuda24@users.noreply.github.com>
2024-07-09 23:47:50 +0700

    First Commit

---
+++ Assets/Script/BallControl.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private float rand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        Invoke("GoBall", 0.2f);
+    }
+    void GoBall()
+    {
+        rand = Random.Range(0, 2);
+        if (rand == 0)
+        {
+            rb2d.AddForce(new Vector2(14, -8));
+        }
+        else
+        {
+            rb2d.AddForce(new Vector2(-14, -8));
+        }
+    }
+
+    public void Restart()
+    {
+        Debug.Log("Game Restarted");
+        Reset();
+        Invoke("GoBall", 0.1f);
+    } 
+    private void Reset()
+    {
+        rb2d.velocity = Vector2.zero;
+        transform.position = Vector2.zero;
+    }
+
+    [SerializeField] private int stageCollisionCount;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            rb2d.AddForce(new Vector2(14, -8));
+            stageCollisionCount = 0;
+        } else if(collision.gameObject.name == "Bear")
+        {
+            rb2d.AddForce(new Vector2(-14, -8));
+            stageCollisionCount = 0;
+        }
+        else 
+        {
+            stageCollisionCount++;
+            Debug.Log("Stage Collision = " + stageCollisionCount);
+            if(stageCollisionCount > 6)
+            {
+                GoBall();
+            }
+        }
+    }
+
+}
